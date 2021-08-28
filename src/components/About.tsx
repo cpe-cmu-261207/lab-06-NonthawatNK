117835572031690d3f93776bb7828c6cdf41c487
NonthawatNK <66126171+NonthawatNK@users.noreply.github.com>
2021-08-28 14:22:42 +0700

    f my lastname is broken

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,7 +6,7 @@ const About = () => {
     return (
         <div className='text-center space-y-3'>
             <p className='text-2xl font-semibold'>About me</p>
-            <p className='text-xl'>Nonthawat Kongsicahi 610610547</p>
+            <p className='text-xl'>Nonthawat Kongsichai 610610547</p>
         </div>
     )
 }
